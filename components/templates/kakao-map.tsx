64eb9bc3de356d659230bed7e0d3534937201076
kionys <kiwon7271@naver.com>
2024-08-18 18:23:39 +0900

    update: 공공데이터 마커 표출

--- components/templates/kakao-map.tsx
+++ components/templates/kakao-map.tsx
@@ -1,8 +1,10 @@
 /*global kakao */
+import * as stores from "@core/data/store_data.json";
 import Script from "next/script";
 
 const KAKAO_MAP_API_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`;
-
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
 declare global {
   interface Window {
     kakao: any;
@@ -10,16 +12,6 @@ declare global {
 }
 
 export const KakaoMap = () => {
-  const loadKakaoMap = () => {
-    window.kakao.maps.load(() => {
-      const mapContainer = document.getElementById("map");
-      const mapOption = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
-      };
-      new window.kakao.maps.Map(mapContainer, mapOption);
-    });
-  };
   return (
     <>
       <Script
@@ -31,4 +23,35 @@ export const KakaoMap = () => {
       <div id="map" className="w-full h-screen" />
     </>
   );
+};
+
+const loadKakaoMap = () => {
+  // 지도 로드
+  window.kakao.maps.load(() => {
+    const mapContainer = document.getElementById("map");
+    const mapOption = {
+      center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+      level: 3,
+    };
+
+    // 지도 띄우기
+    const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+    // 식당 데이터 마커 띄우기
+    stores?.DATA?.map(store => {
+      // 마커가 표시될 위치
+      var markerPosition = new window.kakao.maps.LatLng(
+        store?.y_dnts,
+        store?.x_cnts,
+      );
+
+      // 마커를 생성
+      var marker = new window.kakao.maps.Marker({
+        position: markerPosition,
+      });
+
+      // 마커가 지도 위에 표시되도록 설정
+      marker.setMap(map);
+    });
+  });
 };
