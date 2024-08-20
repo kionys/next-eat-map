cdc3822fcf4b40e6acb97d4d131cef51a3bb972c
kionys <kiwon7271@naver.com>
2024-08-20 17:01:45 +0900

    chore: store.ts 타입 재정의

--- core/interfaces/store.ts
+++ core/interfaces/store.ts
@@ -1,71 +1,18 @@
-export interface IStoreData {
-  bizcnd_code: string | null;
-  bizcnd_code_nm: string | null;
-  cgg_code: string | null;
-  cgg_code_nm: string | null;
-  cob_code: string | null;
-  cob_code_nm: string | null;
-  crt_time: number | null;
-  crtfc_class: string | number | null;
-  crtfc_gbn: string | null;
-  crtfc_gbn_nm: string | null;
-  crtfc_sno: string | null;
-  crtfc_upso_mgt_sno: number | null;
-  crtfc_ymd: string | null;
-  crtfc_yn: string | null;
-  food_menu: string | number | null;
-  gnt_no: string | number | null;
-  map_indict_yn: string | null;
-  owner_nm: string | null;
-  rdn_addr_code: string | number | null;
-  rdn_code_nm: string | null;
-  rdn_detail_addr: string | number | null;
-  tel_no: string | number | null;
-  upd_time: number | null;
-  upso_nm: string | null;
-  upso_sno: string | number | null;
-  use_yn: string;
-  x_cnts: string;
-  y_dnts: string;
-}
-export interface IStore extends IStoreData {
-  // tel_no: string | number | null;
-  // cob_code_nm: string | null;
-  // bizcnd_code_nm: string | null;
-  // upso_nm: string | null;
-  // x_cnts: string;
-  // y_dnts: string;
-  // rdn_code_nm: string | null;
-  // crtfc_gbn_nm: string | null;
+export interface IStore {
   id: number;
-  phone: string | number | null;
-  storeType: string | null;
-  category: string | null;
-  name: string | null;
-  lng: string;
-  lat: string;
-  address: string | null;
-  foodCertifyName: string | null;
+  phone?: string | null;
+  storeType?: string | null;
+  category?: string | null;
+  name?: string | null;
+  lng?: string | null;
+  lat?: string | null;
+  address?: string | null;
+  foodCertifyName?: string | null;
+}
 
-  // bizcnd_code: string | null;
-  // cgg_code: string | null;
-  // cgg_code_nm: string | null;
-  // cob_code: string | null;
-  // crt_time: number | null;
-  // crtfc_class: string | number | null;
-  // crtfc_gbn: string | null;
-  // crtfc_sno: string | null;
-  // crtfc_upso_mgt_sno: number | null;
-  // crtfc_ymd: string | null;
-  // crtfc_yn: string | null;
-  // food_menu: string | number | null;
-  // gnt_no: string | number | null;
-  // map_indict_yn: string | null;
-  // owner_nm: string | null;
-  // rdn_addr_code: string | number | null;
-  // rdn_detail_addr: string | number | null;
-
-  // upd_time: number | null;
-  // upso_sno: string | number | null;
-  // use_yn: string;
+export interface IStoreApiResponse {
+  data: IStore[];
+  totalPage?: number;
+  totalCount?: number;
+  page?: number;
 }
