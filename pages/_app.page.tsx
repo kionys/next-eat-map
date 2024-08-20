95ec87f693643739516fb3a579df7593d61fa9ab
kionys <kiwon7271@naver.com>
2024-08-20 17:01:04 +0900

    update: react-query provider 적용

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,12 +2,19 @@ import "@components/styles/globals.css";
 import { Layout } from "components/layout";
 
 import type { AppProps } from "next/app";
-export default function App({ Component, pageProps }: AppProps) {
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
-}
+};
+export default App;
