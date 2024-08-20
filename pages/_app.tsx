f0c8c8ecc2e7828aed6cec24169017010721eb44
kionys <kiwon7271@naver.com>
2024-08-20 22:50:38 +0900

    update: next-auth provider와 session을 _app.tsx에 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
 import "@components/styles/globals.css";
 import { Layout } from "components/layout";
-
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
+const queryClient = new QueryClient();
+
 const App = ({ Component, pageProps }: AppProps) => {
-  const queryClient = new QueryClient();
+  const { session } = pageProps;
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <ReactQueryDevtools />
+      {/* next auth - session provider */}
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools />
+      </SessionProvider>
     </QueryClientProvider>
   );
 };
