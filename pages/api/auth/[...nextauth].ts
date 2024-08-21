20ac5649cb5916b7cccc5f845a874ac8fffcbaf3
kionys <kiwon7271@naver.com>
2024-08-21 18:26:53 +0900

    update: login 화면 마크업 및 네이버 로그인 인증 로직 추가

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
+import NaverProvider from "next-auth/providers/naver";
 
 const prisma = new PrismaClient();
 
@@ -17,6 +18,13 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID || "",
+      clientSecret: process.env.NAVER_CLIENT_SECRET || "",
+    }),
   ],
+  pages: {
+    signIn: "/users/login", // signIn 함수를 실행하면 /users/login로 리다이렉트 시킨다.
+  },
 };
 export default NextAuth(authOptions);
