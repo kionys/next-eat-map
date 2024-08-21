365221b4e1dc00a36a8ffecdae58404b49cf700d
kionys <kiwon7271@naver.com>
2024-08-22 00:38:43 +0900

    fix: 파일명으로 인한 라우트 이슈 해결

--- pages/stores/add/index.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * 맛집 생성 페이지
- * @author 김기원
- */
-const StoreAddPage = () => {
-  return <>Store Add</>;
-};
-export default StoreAddPage;
---
+++ pages/stores/new/index.tsx
@@ -0,0 +1,8 @@
+/**
+ * 맛집 생성 페이지
+ * @author 김기원
+ */
+const StoreNewPage = () => {
+  return <>Store New</>;
+};
+export default StoreNewPage;
