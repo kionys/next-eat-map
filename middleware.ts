7313119b99552550a37776ef502e40eba5576762
kionys <kiwon7271@naver.com>
2024-08-22 00:00:54 +0900

    chore: middleware 설명 주석 추가

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
+  // matcher에 추가된 경로는 로그인이 필수로 들어가도록 설정된다.
   matcher: ["/users/mypage", "/stores/new", "/stores/:id/edit", "/users/likes"],
 };
