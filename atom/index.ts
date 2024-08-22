611a9044f0746f39933e61ec17f3305c1bf00493
kionys <kiwon7271@naver.com>
2024-08-22 22:42:52 +0900

    add: recoil > atom .ts 추가

---
+++ atom/index.ts
@@ -0,0 +1,26 @@
+import { ILocation, IStore } from "@core/interfaces/store";
+import { atom } from "recoil";
+
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
+const DEFAULT_ZOOM = 5;
+
+export const mapState = atom({
+  key: "map",
+  default: null,
+  dangerouslyAllowMutability: true,
+});
+
+export const currentStoreState = atom<IStore | null>({
+  key: "store",
+  default: null,
+});
+
+export const locationState = atom<ILocation>({
+  key: "location",
+  default: {
+    lat: DEFAULT_LAT,
+    lng: DEFAULT_LNG,
+    zoom: DEFAULT_ZOOM,
+  },
+});
